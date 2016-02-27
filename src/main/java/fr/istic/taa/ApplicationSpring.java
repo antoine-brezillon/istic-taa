2a0065ccb34551bb84ee69155ba697429d052e0e
Antoine Brezillon <antoine.brezillon@gmail.com>
2016-02-27 18:06:48 +0100

    fix database generation

--- src/main/java/fr/istic/taa/ApplicationSpring.java
+++ src/main/java/fr/istic/taa/ApplicationSpring.java
@@ -1,6 +1,7 @@
 package fr.istic.taa;
 
 
+import com.google.common.base.Joiner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +17,20 @@ public class ApplicationSpring {
 
     public static void main( String[] args ) throws UnknownHostException {
         SpringApplication app = new SpringApplication(ApplicationSpring.class);
+        addLiquibaseScanPackages();
         app.run(args);
+    }
+
+    /**
+     * Set the liquibases.scan.packages to avoid an exception from ServiceLocator.
+     */
+    private static void addLiquibaseScanPackages() {
+        System.setProperty("liquibase.scan.packages", Joiner.on(",").join(
+                "liquibase.change", "liquibase.database", "liquibase.parser",
+                "liquibase.precondition", "liquibase.datatype",
+                "liquibase.serializer", "liquibase.sqlgenerator", "liquibase.executor",
+                "liquibase.snapshot", "liquibase.logging", "liquibase.diff",
+                "liquibase.structure", "liquibase.structurecompare", "liquibase.lockservice",
+                "liquibase.ext", "liquibase.changelog"));
     }
 }
